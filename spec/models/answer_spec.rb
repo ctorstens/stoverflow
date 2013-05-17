7fc57d49ca478557b557e64c2d66eb65ec20e3d0
Chris Torstenson <itunes.torstenson@gmail.com>
2013-05-16 22:33:47 -0700

    add some pending specs

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Vote do
-  
+
+  context "valid input" do
+
+    it "should be able to create"
+
+  end
+
+  context "invalid input" do
+
+    it "should not allow users to vote more than once"
+
+    it "should fail if content doesn't exist"
+
+  end
+
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Vote do
-  
+
+  it "can be created with valid input"
+
+  it "can't be created with invalid input"
+
 end
