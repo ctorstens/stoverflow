461b65a40b00a6ff07ba0e287f1279a3c5386e3f
vincentstorme <storme.vincent@gmail.com>
2013-05-16 19:32:24 -0700

    added session creation on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      session[:id] = @user.id
       redirect_to "/users/#{@user.handle}"
     else
       render :new
