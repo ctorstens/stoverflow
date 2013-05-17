3e2628896088c5c6687be8d9866c7b1fa1668ca2
vincentstorme <storme.vincent@gmail.com>
2013-05-16 20:47:39 -0700

    cleaned up controllers again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    puts 'we are here******************'
-
     @user = User.new(params[:user])
     if @user.save
       sign_in
