2cce4d46f1846a72a5657a6616e2453af1466ece
vincentstorme <storme.vincent@gmail.com>
2013-05-16 19:25:53 -0700

    added signup route; also fixed arch finally

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Stoverflow::Application.routes.draw do
   match '/' => 'questions#index'
   match '/login' => 'logins#new'
   match '/logout' => 'logins#delete'
+  match '/signup' => 'users#new'
 
 
-  resources :users,  :only => [:new, :create, :show]
+  resources :users,  :only => [:create, :show]
   resources :logins, :only => [:create]
   resources :questions
   resources :answers
