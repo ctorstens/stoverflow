9066c927e6ed899024e3e4338df09205e1deafd6
Chris Torstenson <itunes.torstenson@gmail.com>
2013-05-16 17:07:06 -0700

    fix spec validation for 'handle'

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:handle)  {|n| "pirate-#{n}"}
+    sequence(:handle) {|n| "pirate_#{n}"}
     sequence(:email) {|n| "parrot-#{n}@tpb.org" }
     password 'blackbeard'
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,55 +3,56 @@ require 'spec_helper'
 describe User do
   let(:user) { build(:user) }
 
-  describe '#Validations' do
+  context 'valid input' do
     it 'validates the length of a good handle' do
       user.should be_valid
     end
-    it 'validates the length of a bad handle' do
-      user = build(:user, :handle => '')
-      user.should_not be_valid
-    end
-
 
     it 'validates the length of a good email' do
       user.should be_valid
     end
+
+    it 'validates the length of a good password' do
+      user.should be_valid
+    end
+
+    it 'validates the format of a good email' do
+      user.should be_valid
+    end
+
+  end
+
+  context "invalid input" do
+
+    it 'validates the format of a bad email' do
+      user = build(:user, :email => 'Pat Smells Lovely')
+      user.should_not be_valid
+    end
+
     it 'validates the length of a bad email' do
       user = build(:user, :email => '')
       user.should_not be_valid
     end
 
-
-    it 'validates the length of a good password' do
-      user.should be_valid
-    end
-    it 'validates the length of a bad password' do
-      user = build(:user, :password => '')
+    it 'validates the length of a bad handle' do
+      user = build(:user, :handle => '')
       user.should_not be_valid
     end
 
-
-    it 'validates the format of a good email' do
-      user.should be_valid
-    end
-    it 'validates the format of a bad email' do
-      user = build(:user, :email => 'Pat Smells')
-      user.should_not be_valid
-    end
-
-
-    it 'validates the uniqueness of a bad email' do
-      pending
-    end
+    it 'validates the uniqueness of a bad email'
 
   end
 
-  describe '#Authentication' do
+  context '#Authentication' do
+
     it 'checks the authentication of the user' do
       user.authenticate('blackbeard').should be_true
     end
+
     it 'checks the authentication of a bad user' do
       user.authenticate('NOT VALID PASSWORD').should be_false
     end
+
   end
+
 end
