b0efd5b1b752ba8ca918a6195c3ac38ee3ddfb90
Chris Torstenson <itunes.torstenson@gmail.com>
2013-05-17 18:23:22 -0700

    create vote controller spec with rudimentary pendings

---
+++ spec/controllers/votes_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Vote do
+
+  it "should pop up a login/register window if no user logged in"
+
+  it "should create vote if user logged in"
+
+  it "should allow user to change vote"
+
+  it "should change attribute score based upon vote up or down"
+
+  it "should only increment, or decrement 1, unless awesome"
+
+end
